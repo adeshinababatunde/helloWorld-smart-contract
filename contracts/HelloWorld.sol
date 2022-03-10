a5c4a07ebb7707dafc49d54ea7e0cec8ee1edf9a
adeshinababatunde <adebuleadeshina10@gmail>
2022-03-10 07:16:08 +0100

    first commit

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -9,10 +9,13 @@ contract HelloWorld {
     constructor(string memory _greeting) {
 
         greeting = _greeting;
-    };
+    }
 
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
 
     function setGreeting(string memory _newGreeting) public {
         greeting = _newGreeting;
-    };
-};
+    }
+}
